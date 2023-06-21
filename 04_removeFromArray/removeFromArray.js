b73b254d7918e8ed8296e806f9f0240475e39d99
dev-spectre <abhishekdallasalpy@gmail.com>
2023-06-21 21:18:00 +0530

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...target) {
+    return array.filter(value => !target.includes(value))
 };
 
 // Do not edit below this line
