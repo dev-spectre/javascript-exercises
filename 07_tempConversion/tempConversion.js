d29bf33d3019c355b01aae7ceb2f29b1dec1f532
dev-spectre <abhishekdallasalpy@gmail.com>
2023-06-21 22:32:41 +0530

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  const tempInCelsius = (tempInFahrenheit - 32) * 5/9;
+  return Math.round(tempInCelsius * 1e1) / 1e1;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  const tempInFahrenheit = tempInCelsius * 9/5 + 32;
+  return Math.round(tempInFahrenheit * 1e1) / 1e1;
 };
 
 // Do not edit below this line
