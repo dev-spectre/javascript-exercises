78a43bee56dc0fa0ee1c3d26d93698939c5cd35b
dev-spectre <abhishekdallasalpy@gmail.com>
2023-06-21 21:59:20 +0530

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (
+        typeof start != 'number' ||
+        typeof end != 'number' ||
+        start < 0 || end < 0 
+    ) return 'ERROR';
 
+    //* naive solution
+    // let sum = 0;
+    // for (let i = Math.min(start, end); i <= Math.max(start, end); i++) {
+    //     sum += i;
+    // }
+
+    //* sum of arithmetic progression = (no. of terms)*(first term + last term)/2
+    const sum = (Math.abs(start - end) + 1) * (start + end) / 2 
+    return sum;
 };
 
 // Do not edit below this line
