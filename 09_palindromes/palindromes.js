725ae0559145a984ce6a3b2d6f25a1a4f19dad3a
dev-spectre <abhishekdallasalpy@gmail.com>
2023-08-08 18:01:36 +0530

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const ALPHANUMERICS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPRSTUVWXYZ1234567890";
+  const alphaString = string
+                        .toLowerCase()
+                        .split("")
+                        .filter((char) => ALPHANUMERICS.includes(char))
+                        .join("");
+  return alphaString === alphaString.split("").reverse().join("");
 };
 
 // Do not edit below this line
