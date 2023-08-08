7716738621d884518b59a8fd1a2c1c877b360e1b
dev-spectre <abhishekdallasalpy@gmail.com>
2023-08-08 18:13:27 +0530

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if (num < 0) return "OOPS";
+  if (num == 1 || num == 2) return 1;
+  const memo = {};
+  if (num in memo) return memo[num];
+  const fib = fibonacci(num - 1) + fibonacci(num - 2);
+  memo[num] = fib;
+  return fib;
 };
 
 // Do not edit below this line
