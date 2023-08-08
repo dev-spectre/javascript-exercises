030d23ba4b9f627879f110a6fc9e761c449e409d
dev-spectre <abhishekdallasalpy@gmail.com>
2023-08-08 17:32:40 +0530

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, num) => {
+    sum += num;
+    return sum;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  return args.reduce((product, num) => {
+    product *= num;
+    return product
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) return 1;
+  let fact = 1;
+  for (let i = 1; i <= num; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
