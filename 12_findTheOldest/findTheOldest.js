3f1c0cb12e8213621fca8a74af06a547e2fc3e87
dev-spectre <abhishekdallasalpy@gmail.com>
2023-08-08 18:34:46 +0530

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people.reduce((oldest, person) => {
+    const lastYearOfPerson =
+      "yearOfDeath" in person
+        ? person.yearOfDeath
+        : new Date().getFullYear();
+    const lastYearOfOldest =
+      "yearOfDeath" in oldest
+        ? oldest.yearOfDeath
+        : new Date().getFullYear();
+    const ageOfPerson = lastYearOfPerson - person.yearOfBirth;
+    const ageOfOldest = lastYearOfOldest - oldest.yearOfBirth;
+    if (ageOfPerson > ageOfOldest) oldest = person;
+    return oldest;
+  }, people[0]);
 };
 
 // Do not edit below this line
